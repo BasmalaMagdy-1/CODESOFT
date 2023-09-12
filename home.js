39ff8ef6977af21579975f0c7bd4579e4aa202f6
Fatma Mahmoud <mahmoudfatma3060@gmail.com>
2023-09-13 00:16:48 +0200

    portifolio-task

---
+++ home.js
@@ -0,0 +1,13 @@
+var type = new Typed(".typing", {
+    strings: [ "Software engineer","Competitive problem solver","Web developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+  var type = new Typed(".type", {
+    strings: [ "Skills"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    loop: true
+  });
